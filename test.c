87060035f397105b7fdd5a3b038fa5eada9e1ed6
YuriJT <Yuri.r.tobias@hotmail.com.br>
2023-11-04 10:48:00 -0300

    [ADD]: functions iniciaAlocador and finalizaAlocador

---
+++ test.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+extern int sum(int, int);
+
+extern int sum_2(int);
+
+extern void *iniciaAlocador();
+
+extern void *finalizaAlocador();
+
+extern void * topoInicialHeap;
+
+extern void * aposAlteracao;
+
+extern void * resetaHeap;
+
+int main() {
+    int c;
+    
+    // printf("%d\n", sum(15, 15));
+    // printf("%d\n", sum_2(8));
+    // printf("%p\n", iniciaAlocador());
+    // topoInicialHeap = iniciaAlocador();
+    iniciaAlocador();
+    printf("%p\n", topoInicialHeap);
+    finalizaAlocador();
+    printf("%p\n", aposAlteracao);
+    printf("%p\n", resetaHeap);
+
+    return 0;
+}
---
+++ test.s
@@ -0,0 +1,88 @@
+.section .data
+    topoInicialHeap:   .quad 0
+    aposAlteracao:     .quad 0
+    resetaHeap:        .quad 0 
+    formatString:      .string "Valor da brk: %p\n"
+    formatStringInit:  .string "Iniciando printf...\n"  
+.section .text
+    .global sum
+    .global sum_2
+    .global iniciaAlocador
+    .global finalizaAlocador
+    .global topoInicialHeap
+    .global aposAlteracao
+    .global resetaHeap
+
+.globl _start
+
+iniciaAlocador:
+    pushq %rbp
+    movq %rsp, %rbp
+
+    # Preparando a pilha para a chamada do printf
+    subq $8, %rbp
+
+    # Chama printf para exibir uma mensagem
+    leaq formatStringInit(%rip), %rdi
+    call printf
+
+    # Restaurando a pilha
+    addq $8, %rbp
+
+    # Pega o valor inicial do brk vide tabela presente no livro
+    movq $12, %rax
+    movq $0, %rdi
+    syscall
+
+    # Armazena o valor obtido na variavel global topoIniciaHeap
+    # O (%rip) eh util por conta do enderecamento relativo
+    movq %rax, topoInicialHeap(%rip)
+
+    popq %rbp
+    ret
+
+finalizaAlocador:
+    pushq %rbp
+    movq %rsp, %rbp
+
+    # Pega o valor atual
+    movq $12, %rax
+    movq $0, %rdi
+    syscall
+
+    # Soma 2048 ao valor atual e chama o syscall pra atualizar a brk
+    movq %rax, %rdi
+    movq $12, %rax
+    addq $2048, %rdi
+    syscall
+
+    # Armazena o novo valor da brk na variavel global aposAlteracao
+    movq %rax, aposAlteracao(%rip)
+
+    # Reseta o valor inicial da brk
+    movq $12, %rax
+    movq topoInicialHeap(%rip), %rdi
+    syscall
+
+    # Armazena o novo valor da brk na variavel global resetaHeap
+    movq %rax, resetaHeap(%rip)
+
+    popq %rbp
+    ret
+
+sum:
+    pushq %rbp
+    movq %rsp, %rbp
+    movq %rdi, %rax
+    movq %rsi, %rbx
+    addq %rbx, %rax
+    popq %rbp
+    ret
+
+sum_2:
+    pushq %rbp
+    movq %rsp, %rbp
+    movq %rdi, %rax
+    addq $2, %rax
+    popq %rbp
+    ret
